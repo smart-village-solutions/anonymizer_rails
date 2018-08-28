cc3e03ee41a3107e14baf6171e3387ae6f386c64
Sebastian Peter <sebastian.peter@ikusei.de>
2018-08-28 19:30:19 +0200

    Moved simplecov to top of spechelper to include all necessary files in TestCoverage

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-require 'simplecov'
-SimpleCov.start
+
 ENV['RAILS_ENV'] ||= 'test'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 ENV['RAILS_ENV'] ||= 'test'
-
+require 'simplecov'
+SimpleCov.start
 
 require File.expand_path("../dummy/config/environment.rb", __FILE__)
 require 'rspec/rails'
